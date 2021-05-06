7856cced51e44680468595791b6b2fa1a77a6e27
Peiran Quan <Peiran.Quan@southerncross.co.nz>
2021-05-06 16:28:16 +1200

    test some sale price is equal to 0

--- models/snowflake_test_data/snowflake_model_1.sql
+++ models/snowflake_test_data/snowflake_model_1.sql
@@ -1,6 +1,7 @@
 with snow_source as (
     select SS_SALES_PRICE as sale_price
     from {{source('snowflake_sample_data_ref', 'store_sales')}}
+    where sale_price <= 0
 )
 
 select * from snow_source
